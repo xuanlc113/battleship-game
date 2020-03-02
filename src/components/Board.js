7205a64b3ae47772b447fe61d69729848b56b41c
Paperplastic <xxl-0011@hotmail.com>
2020-03-02 16:53:40 +0800

    added cell click function

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Ship from "./Ship";
 import Cell from "./Cell";
 import "../styles/Board.css";
 
 const Board = props => {
+  const [render, setRender] = useState(true);
+  useEffect(() => {
+    console.log("touvh");
+  }, [props.board.hit, props.board.miss]);
+
+  function clickCell(pos) {
+    props.board.receiveAttack(pos);
+    setRender(prevState => !prevState);
+  }
+
   function renderBoard() {
     let cell = [];
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
         if (props.board.hit.includes([i, j])) {
-          cell.push(<Cell style="hit" />);
+          cell.push(<Cell style="hit" pos={[i, j]} clickCell={clickCell} />);
         } else if (props.board.miss.includes([i, j])) {
-          cell.push(<Cell style="miss" />);
+          cell.push(<Cell style="miss" pos={[i, j]} clickCell={clickCell} />);
         } else {
-          cell.push(<Cell style="empty" />);
+          cell.push(<Cell style="empty" pos={[i, j]} clickCell={clickCell} />);
         }
       }
     }
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,7 +2,12 @@ import React from "react";
 import "../styles/Cell.css";
 
 const Cell = props => {
-  return <div className={`cell ${props.style}`}></div>;
+  return (
+    <div
+      className={`cell ${props.style}`}
+      onClick={() => props.clickCell(props.pos)}
+    ></div>
+  );
 };
 
 export default Cell;
