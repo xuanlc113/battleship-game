25e0a8329eaa854a397d5f2eb64bb8e43e4eaf59
Paperplastic <xxl-0011@hotmail.com>
2020-03-02 14:39:47 +0800

    added ships with fixed location

---
+++ src/utils/helper.js
@@ -0,0 +1,15 @@
+function randomizeBoard() {
+  let ships = [];
+  ships = [
+    Ship(4, 0, 0, true),
+    Ship(4, 2, 3, false),
+    Ship(3, 5, 6, true),
+    Ship(3, 1, 6, false),
+    Ship(2, 7, 7, false),
+    Ship(1, 8, 1, true),
+    Ship(1, 8, 9, true)
+  ];
+  return ships;
+}
+
+function randomShip() {}
