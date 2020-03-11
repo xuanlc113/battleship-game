2dd8bed1bb1eb92ca255fa54bc0c1e5fb8c086f7
Paperplastic <xxl-0011@hotmail.com>
2020-03-11 15:26:19 +0800

    Updates

---
+++ precache-manifest.e893454b8bd70dfb940cd08ccde6e944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1208cac87c13d095c64e2b7ee0052bb",
+    "url": "/battleship-game/index.html"
+  },
+  {
+    "revision": "dd03f3548f21062fabb4",
+    "url": "/battleship-game/static/css/main.f2d26e65.chunk.css"
+  },
+  {
+    "revision": "65e099912c238cf7a5cb",
+    "url": "/battleship-game/static/js/2.e4303854.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/battleship-game/static/js/2.e4303854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd03f3548f21062fabb4",
+    "url": "/battleship-game/static/js/main.bdce238b.chunk.js"
+  },
+  {
+    "revision": "a8d491d1d73f68477439",
+    "url": "/battleship-game/static/js/runtime-main.fcb207a7.js"
+  }
+]);
